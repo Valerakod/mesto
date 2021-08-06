c89abd321b7c6e386e72a3218f63ee5dd2ae8433
Valeriya Yemets <valerakod30@gmail.com>
2021-08-06 20:40:25 +0300

    Моя червётная проектная работа

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,98 @@
 let editButton = document.querySelector(".profile__edit-button");
-let editPopup = document.querySelector(".popup");
+let popup1 = document.getElementById("popup1");
 let saveButton = document.querySelector(".popup__save-button");
 let closeButton = document.querySelector(".popup__close-button");
 let formElement = document.querySelector(".popup__form");
-let nameInput = document.querySelector(".popup__text_check_name");
-let jobInput = document.querySelector(".popup__text_check_job");
+let nameInput = document.getElementById("popup-text-check-name");
+let jobInput = document.getElementById("popup-text-check-job");
 let profileName = document.querySelector(".profile__name");
 let profileJob = document.querySelector(".profile__job");
+let addButton = document.querySelector(".profile__add-button");
+let addPopup = document.getElementById("popup2");
+let placeNameInput = document.getElementById("popup-text-place-name");
+let placeSourceInput = document.getElementById("popup-text-place-source");
+let textElement = document.querySelector(".element__text");
+let cardImage = document.querySelector(".element__image");
+let popup3 = document.getElementById("popup3");
+let like = document.querySelector(".element__heart");
 
-function openPopup() {
+
+function openPopup1() {
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
-  editPopup.classList.add("popup_opened");
+  popup1.classList.add("popup_opened");
 }
 
-function closePopup() {
-  editPopup.classList.remove("popup_opened");
+function closePopup1() {
+  popup1.classList.remove("popup_opened");
 }
 
-editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
+function openPopup2() {
+  popup2.classList.add("popup_opened");
+}
+
+function closePopup2() {
+  popup2.classList.remove("popup_opened");
+}
+
+function openPopup3() {
+  popup3.classList.add("popup_opened");
+}
+
+function closePopup3() {
+  popup3.classList.remove("popup_opened");
+}
+
+function likeActive() {
+  like.classList.toggle("element__heart_active");
+}
+
+editButton.addEventListener('click', openPopup1);
+closeButton.addEventListener('click', closePopup1);
+addButton.addEventListener('click', openPopup2);
+closeButton.addEventListener('click', closePopup2);
+cardImage.addEventListener('click', openPopup3);
+closeButton.addEventListener('click', closePopup3);
+like.addEventListener('click', likeActive);
+
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  closePopup();
+  closePopup1();
+  closePopup2();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
+
+
+
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
